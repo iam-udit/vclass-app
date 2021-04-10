0c6e28a9987335c1d10e22788f0b4435e7dbf138
Udit Narayan Nayak <iamudit.nayak@gmail.com>
2021-04-10 10:33:52 +0530

    Added news model class

---
+++ src/main/java/com/mindtree/vclass/model/News.java
@@ -0,0 +1,109 @@
+package com.mindtree.vclass.model;
+
+import java.sql.Date;
+
+/**
+ * This class is used to implement the news operation
+ * 
+ * @author D-HDKR
+ * @version 1.0
+ */
+public class News implements Model {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Properties of News
+	 */
+	private int id;
+	private String title;
+	private String description;
+	private Date createdAt;
+	private Date updatedAt;
+	
+	/**
+	 * Instantiate the News 
+	 */
+	public News() {
+		super();
+	}
+
+	/**
+	 * Initialize the news properties
+	 * 
+	 * @param id
+	 * @param title
+	 * @param description
+	 * @param createdAt
+	 * @param updatedAt
+	 */
+	public News(String title, String description) {
+		super();
+		this.title = title;
+		this.description = description;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	/**
+	 * @return the createdAt
+	 */
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	/**
+	 * @return the updatedAt
+	 */
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+}
