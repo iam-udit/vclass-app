ee334c2bc7bfa46be44182aa6e8f0c6920d929f0
Udit Narayan Nayak <iamudit.nayak@gmail.com>
2021-04-15 21:09:48 +0530

    Added the DB SQL query

--- src/main/java/com/mindtree/vclass/dao/DB-QUERY.sql
+++ src/main/java/com/mindtree/vclass/dao/DB-QUERY.sql
@@ -18,6 +18,19 @@ CREATE TABLE IF NOT EXISTS users (
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
+# Insert root admin details
+INSERT INTO users(name, age, username, password) 
+VALUES("Mr. Admin", 21, "admin@vclass.com", "Admin@123");
+
+# Insert root staff details
+INSERT INTO users(name, age, username, password) 
+VALUES("Mr. Staff", 21, "staff@vclass.com", "Staff@123");
+
+# Insert root student details
+INSERT INTO users(name, age, username, password) 
+VALUES("Mr. Student", 21, "student@vclass.com", "Student@123");
+
+
 # Create role table 
 CREATE TABLE IF NOT EXISTS roles (
 	id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -25,6 +38,11 @@ CREATE TABLE IF NOT EXISTS roles (
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+# Insert all the role for V-Class app
+INSERT INTO roles(name) VALUES("Admin");
+INSERT INTO roles(name) VALUES("Staff");
+INSERT INTO roles(name) VALUES("Student");
 
 # Create user-role table
 CREATE TABLE IF NOT EXISTS role_user (
@@ -35,6 +53,11 @@ CREATE TABLE IF NOT EXISTS role_user (
 	FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
 	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+# Insert the role of admin
+INSERT INTO role_user(user_id, role_id) VALUES(1, 1);
+INSERT INTO role_user(user_id, role_id) VALUES(2, 2);
+INSERT INTO role_user(user_id, role_id) VALUES(3, 3);
 
 # Create address table
 CREATE TABLE IF NOT EXISTS addresses (
@@ -49,6 +72,14 @@ CREATE TABLE IF NOT EXISTS addresses (
 	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+# Insert address of the admin
+INSERT INTO addresses(user_id, city, state, country, pin) 
+VALUES (1, "Bhubaneswar", "Odisha", "India", "756124");
+INSERT INTO addresses(user_id, city, state, country, pin) 
+VALUES (2, "Mumbai", "Madhya Pradesh", "India", "434588");
+INSERT INTO addresses(user_id, city, state, country, pin) 
+VALUES (3, "Bengaluru", "Karnataka", "India", "684922");
+
 # Create news table
 CREATE TABLE IF NOT EXISTS news (
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -56,4 +87,19 @@ CREATE TABLE IF NOT EXISTS news (
 	description TEXT NOT NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+# Create the lessions table
+CREATE TABLE IF NOT EXISTS lessions (
+	id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) UNIQUE NOT NULL,
+    user_id BIGINT UNSIGNED NOT NULL,
+    slug VARCHAR(255) UNIQUE NOT NULL,
+	description TEXT NOT NULL,
+    video VARCHAR(255) NOT NULL,
+    is_published BOOLEAN NOT NULL,
+    is_approved BOOLEAN NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
