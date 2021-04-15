d5cc7ac64ac83151bc31e878168150530bb6a16e
Udit Narayan Nayak <iamudit.nayak@gmail.com>
2021-04-15 22:01:07 +0530

    Added staff's setting controller to handle staff profile operations

---
+++ src/main/java/com/mindtree/vclass/controller/staff/SettingController.java
@@ -0,0 +1,150 @@
+package com.mindtree.vclass.controller.staff;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mindtree.vclass.exception.ServiceException;
+import com.mindtree.vclass.model.User;
+import com.mindtree.vclass.service.Service;
+import com.mindtree.vclass.service.UserService;
+
+/**
+ * Servlet implementation class SettingController is used to perform 
+ * operation on admin profile
+ * 
+ * @author D-HDKR 
+ * @version 1.0
+ */
+@WebServlet(urlPatterns = { "/staff/profile/edit", "/staff/profile/update" })
+
+public class SettingController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SettingController() {
+        super();
+    }
+
+	/**
+	 * Redirect to admin profile view
+	 * 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
+			
+			// Forward the request to eidt page
+			getServletContext().getRequestDispatcher("/views/staff/profile.jsp")
+			.forward(request, response);
+			
+		} catch (IOException | ServletException e) {
+			
+			// Log the exception messages
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * Update the admin profile details
+	 * 
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+			
+		try {
+		
+			String status = "";
+			Service<User> service = new UserService();
+			
+			// Get user new details
+			User admin = getAdminToUpdate(request, response);
+
+			if (service.update(admin)) {
+
+				request.getSession().setAttribute("user", 
+						service.read(admin.getUsername()));
+				
+				// Return success response
+				status = "Profile updated Successfully !";
+			} else {
+				
+				// Return the error response
+				status = "Profile updation failed !";
+			}	
+				
+			// Redirect to response page
+			response.sendRedirect(request.getContextPath() 
+					+  "/staff/profile/edit?status=" + status);
+			
+		} catch (IOException | ServiceException e) {
+			
+			// Log the exception messages
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	/**
+	 * Get updated admin details 
+	 * 
+	 * @param request
+	 * @param response
+	 * @return	return new student details to be update
+	 */
+	private User getAdminToUpdate(HttpServletRequest request, HttpServletResponse response) {
+		
+		User admin = null;
+
+		try {
+			
+			admin = new User();
+			
+			// Retrieve the user updated value
+			String name = request.getParameter("name");
+			String age = request.getParameter("age");
+			String password = request.getParameter("password");
+			String city = request.getParameter("city");
+			String state = request.getParameter("state");
+			String country = request.getParameter("country");
+			String pin = request.getParameter("pin");
+
+
+			// Retrieve user previous details
+			User adminDetails = (User) request.getSession().getAttribute("user");
+			
+			// Set the user new address details
+			Map<String, String> address = new HashMap<String, String>();
+			address.put("city", (city != null) ? city : adminDetails.getAddress().get("city"));
+			address.put("state", (state != null) ? state : adminDetails.getAddress().get("state"));
+			address.put("country", (country != null) ? country : adminDetails.getAddress().get("country"));
+			address.put("pin", (pin != null) ? pin : adminDetails.getAddress().get("pin"));
+
+			// Set the user new input
+			admin.setId(adminDetails.getId());
+			admin.setName((name != null) ? name : adminDetails.getName());
+			admin.setAge((age != null) ? Byte.parseByte(age) : adminDetails.getAge() );
+			admin.setRole(adminDetails.getRole());
+			admin.setUsername(adminDetails.getUsername());
+			admin.setPassword((password != null) ? password : adminDetails.getPassword());
+			admin.setAddress(address);
+			
+		} catch (NullPointerException | IllegalStateException e) {
+			
+			// Log the exception messages
+			System.out.println(e.getMessage());
+		}
+	
+		return admin;
+	}
+
+}
