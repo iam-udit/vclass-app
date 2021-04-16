dfe2560c7ef8e987b51bf9531a6353c2539d5d9e
Udit Narayan Nayak <iamudit.nayak@gmail.com>
2021-04-16 08:39:39 +0530

    Resolved minor issue by removing unwanted exception thows

--- src/main/java/com/mindtree/vclass/controller/common/LogoutController.java
+++ src/main/java/com/mindtree/vclass/controller/common/LogoutController.java
@@ -33,8 +33,7 @@ public class LogoutController extends HttpServlet {
 	 * 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, 
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 		
 		try {
 			
